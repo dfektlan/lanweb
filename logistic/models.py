90e3f2c1b977c9debe82c18161fa2b785b69a482
kradalby <kradalby@kradalby.no>
2013-07-04 22:57:30 +0200

    started to build logistics input

--- logistic/models.py
+++ logistic/models.py
@@ -13,7 +13,7 @@ class Item(models.Model):
     itemgroup = models.ForeignKey(ItemGroup)
     brand = models.CharField(_("Merke"), max_length=200)
     product_model = models.CharField(_("Produkt modell"), max_length=200)
-    description = models.TextField(_("Beskrivelse"))
+    description = models.CharField(_("Beskrivelse"), max_length=30)
     holder = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, default=None, verbose_name="Current holder", related_name ="Holder", editable=True)
 
     def __unicode__(self):
--- logistic/views.py
+++ logistic/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from logistic.models import ItemGroup, Item
+from logistic.forms import ItemForm
 
 @login_required
 @user_passes_test(lambda u: u.groups.filter(name='Core').count() == 1)
 def index(request):
     items = Item.objects.all()
-    return render(request, 'logistic/index.html', {'items':items})
+    forms = []
+    if request.method == "POST":
+        for i in items:
+            form = ItemForm(request.POST, instance=i)
+            forms.append(form)
+            if form.is_valid():
+                form.save()
+    else: 
+        for i in items:
+            forms.append(ItemForm(instance=i))
+        
+    both = zip(items, forms)
+    return render(request, 'logistic/index.html', {'items':items, 'forms':forms, 'both':both})
