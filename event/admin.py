96d3bb7dec458e6754a95ba517914c967b72062d
Kristoffer Dalby <kradalby@kradalby.no>
2013-07-29 19:28:39 +0200

    added current field and fix

--- event/admin.py
+++ event/admin.py
@@ -4,4 +4,13 @@ from models import LanEvent
 class LanEventAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'start_date', 'end_date',]
 
+    def save_model(self, request, obj, form, change):
+        if obj.current:
+            lanevents = LanEvent.objects.all()
+            for event in lanevents:
+                if event.current:
+                    event.current = False
+                    event.save()
+        obj.save()
+
 admin.site.register(LanEvent, LanEventAdmin)
