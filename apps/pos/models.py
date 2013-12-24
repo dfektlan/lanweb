2312061dcd5eabab1352b8be6051eec3a20c894a
Kristoffer Dalby <kradalby@kradalby.no>
2013-12-24 21:53:34 +0100

    derp and markdown for newwws

--- apps/pos/models.py
+++ apps/pos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import ugettext as _
+from apps.event.models import LanEvent
 
-# Create your models here.
+class ItemGroup(models.Model):
+    name = models.CharField(_('Navn'), max_length=200, blank=False, null=False)
+    date = models.DateTimeField(_('Dato'), editable=False, auto_now_add=True)
+
+class Item(models.Model):
+    name = models.CharField(_('Navn'), max_length=200, blank=False, null=False)
+    date = models.DateTimeField(_('Dato'), editable=False, auto_now_add=True)
+    group = models.ForeignKey(ItemGroup, null=False, verbose_name="Item Group", editable=True)
+    price = models.IntegerField(_('Pris'), blank=False)
+    order = models.ForeignKey()
