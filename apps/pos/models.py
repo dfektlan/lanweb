15992432cb39e6d606c97d601bb3e43f9b8b3d63
Kristoffer Dalby <kradalby@kradalby.no>
2013-12-24 23:24:21 +0100

    added some basics to models

--- apps/pos/models.py
+++ apps/pos/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from apps.event.models import LanEvent
 
+class Order(models.Model):
+    date = models.DateTimeField(_('Dato'), editable=False, auto_now_add=True)
+
 class ItemGroup(models.Model):
     name = models.CharField(_('Navn'), max_length=200, blank=False, null=False)
     date = models.DateTimeField(_('Dato'), editable=False, auto_now_add=True)
@@ -11,4 +14,12 @@ class Item(models.Model):
     date = models.DateTimeField(_('Dato'), editable=False, auto_now_add=True)
     group = models.ForeignKey(ItemGroup, null=False, verbose_name="Item Group", editable=True)
     price = models.IntegerField(_('Pris'), blank=False)
-    order = models.ForeignKey()
+    order = models.ForeignKey(Order, null=True, editable=False)
+
+class ItemPack(models.Model):
+    name = models.CharField(_('Navn'), max_length=200, blank=False, null=False)
+    date = models.DateTimeField(_('Dato'), editable=False, auto_now_add=True)
+    group = models.ForeignKey(ItemGroup, null=False, verbose_name="Item Group", editable=True)
+    #many to many?
+
+
