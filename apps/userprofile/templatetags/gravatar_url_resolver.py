b6af241b4f33946bfafd3585b2eeed566813525b
Kristoffer Dalby <kradalby@kradalby.no>
2014-01-24 23:16:20 +0100

    attempt to fix

--- apps/userprofile/templatetags/gravatar_url_resolver.py
+++ apps/userprofile/templatetags/gravatar_url_resolver.py
@@ -12,9 +12,9 @@ def get_gravatar_url(context, user, size):
 
     #laters
     #prefix = "https://" if context['request'].is_secure() else "http://"
-    default = "http://placehold.it/150x150"
+    default = "https://kradalby.no/placeatle.jpg"
 
-    gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(email).hexdigest() + "?"    
+    gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"    
     gravatar_url += urllib.urlencode({'d': default, 's':str(size)})    
 
     return gravatar_url
