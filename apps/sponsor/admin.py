f9a0272b760ef37873a50003c263389be9a644b5
Kristoffer Dalby <kradalby@kradalby.no>
2014-01-28 19:58:04 +0100

    made it possible to use svg graphics for sponsors

--- apps/sponsor/admin.py
+++ apps/sponsor/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from models import Sponsor
 
 class SponsorAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'event', 'logo', 'url',]
+    list_display = ['name', 'description', 'event', 'url',]
 
 admin.site.register(Sponsor, SponsorAdmin)
