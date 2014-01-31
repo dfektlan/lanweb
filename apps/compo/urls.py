41079d93cba38af5e2b47f7f2a143a8a604d5c51
thormartin91 <thorma@stud.ntnu.no>
2014-01-31 15:40:03 +0100

    Added tournement-view

--- apps/compo/urls.py
+++ apps/compo/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('apps.compo.views',
     url(r'^$', 'overview', name='overview'),
+    url(r'^tournament/(?P<tournament_id>\d+)/$', 'tournament', name='tournament'),
+
 )
--- apps/compo/views.py
+++ apps/compo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from apps.event.models import LanEvent
 from apps.compo.models import Game,Tournament,Participant,Team
+
 
 LATEST_EVENT = LanEvent.objects.filter(current=True)[0]
 
@@ -11,3 +12,7 @@ def overview(request):
     for g in all_games:
         tournament_dict[g] = all_tournaments.filter(game=g)
     return render(request, 'compo/overview.html', {'tournaments':tournament_dict, 'all_games':all_games})
+
+def tournament(request, tournament_id=None):
+    tournament = get_object_or_404(Tournament, pk=tournament_id)
+    return render(request, 'compo/tournament.html', {'tournament':tournament})
