c1b25de42c4d62f8ce93d32f1e495995580ae332
Kristoffer Dalby <kradalby@kradalby.no>
2014-02-13 10:19:09 +0100

    added items to nav

--- apps/compo/urls.py
+++ apps/compo/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('apps.compo.views',
-    url(r'^$', 'overview', name='overview'),
+    url(r'^$', 'overview', name='tournament_overview'),
     url(r'^tournament/(?P<tournament_id>\d+)/$', 'tournament', name='tournament'),
     url(r'^tournament/join/(?P<tournament_id>\d+)/$', 'register_to_tournament', name='register_to_tournament'),
     url(r'^tournament/check/(?P<tournament_id>\d+)/$', 'check_user', name='check_user'),
