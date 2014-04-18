be81be85edd03d9fe474db94ef16f0fe2df7143c
Kristoffer Dalby <kradalby@kradalby.no>
2014-04-18 18:52:41 +0200

    added edit profile option, fixing #95

--- apps/userprofile/urls.py
+++ apps/userprofile/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('apps.userprofile.views',
     url(r'^myprofile', 'myprofile', name='myprofile'),
     url(r'^profile/(?P<user_id>\d+)/$', 'profile', name='profile'),
+    url(r'^editprofile', 'edit_profile', name='edit_profile'),
 )
--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from apps.userprofile.forms import EditProfileForm
 from apps.userprofile.models import SiteUser
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -12,6 +13,7 @@ def profile(request, user_id=None):
 
     return render(request, 'userprofile/profile.html', {'profile': user, 'apps': apps, 'attended': attended})
 
+
 @login_required
 def myprofile(request):
     user = request.user
@@ -19,6 +21,43 @@ def myprofile(request):
     attended = get_all_attended_events(user)
 
     return render(request, 'userprofile/myprofile.html', {'user': user, 'apps': apps, 'attended': attended})
+
+
+@login_required
+def edit_profile(request):
+    user = request.user
+    if request.POST:
+        form = EditProfileForm(request.POST)
+        if form.is_valid():
+            cleaned = form.cleaned_data
+            user.first_name = cleaned["first_name"]
+            user.last_name = cleaned["last_name"]
+            user.email = cleaned["email"]
+            user.address = cleaned["address"]
+            user.zip_code = cleaned["zip_code"]
+            user.town = cleaned["town"]
+            user.country = cleaned["country"]
+            user.phone = cleaned["phone"]
+            user.skype = cleaned["skype"]
+            user.steam = cleaned["steam"]
+            user.save()
+            user.setNameNotRetard()
+            return redirect(myprofile)
+    else:
+        form = EditProfileForm(initial={
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+            "address": user.address,
+            "zip_code": user.zip_code,
+            "town": user.town,
+            "country": user.country,
+            "phone": user.phone,
+            "skype": user.skype,
+            "steam": user.steam
+        })
+    return render(request, 'userprofile/editprofile.html', {'form': form, 'user': user})
+
 
 
 def get_all_attended_events(user):
