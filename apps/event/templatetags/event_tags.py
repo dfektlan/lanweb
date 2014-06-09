9972e5088d1c8e9fc75f9a05b2309a3a54534316
Kristoffer Dalby <kradalby@kradalby.no>
2014-06-09 08:44:01 +0200

    possibly fixed 404 issue

--- apps/event/templatetags/event_tags.py
+++ apps/event/templatetags/event_tags.py
@@ -4,9 +4,21 @@ from apps.event.models import LanEvent
 
 register = template.Library()
 
-@register.inclusion_tag('eventinfo.html')
-#@register.assignment_tag(takes_context=True)
 
-def get_latest_lanevent(event):
+@register.inclusion_tag('eventinfo.html')
+def getEventInformation(event):
     eventObj = LanEvent.objects.get(shortname=event)
     return {'lanevent' : eventObj}
+
+
+
+# This will set the event if it is not set, this is a hack so 404 pages dont give stacktraces. lol
+@register.assignment_tag(takes_context=True)
+def setEventIfNone(context, event):
+    print event
+    if event == "":
+        event = "s14"
+    print event
+    print "IDENTIFY ME"
+    return event
+
