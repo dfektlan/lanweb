46ea33920b1e4a8b547120fa9292221d09729431
Kristoffer Dalby <kradalby@kradalby.no>
2014-06-11 22:54:14 +0200

    fix

--- apps/userprofile/templatetags/gravatar_url_resolver.py
+++ apps/userprofile/templatetags/gravatar_url_resolver.py
@@ -12,7 +12,6 @@ def get_gravatar_url(context, user, size):
 
     default = "https://dfektlan.no/media/nophoto.jpg"
     hash = hashlib.md5(email.lower()).hexdigest()
-    print dir(context)
 
     gravatar_url = "https://www.gravatar.com/avatar/" + hash + "?"    
     gravatar_url += urllib.urlencode({'d': default, 's':str(size)})    
